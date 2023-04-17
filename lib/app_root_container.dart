2bd1fac1716f44e5e70100af2cb944bc191c0889
crablet <love4uandgzfc@gmail.com>
2023-04-18 02:01:37 +0800

    feat: 接入RequiemTransition切换背景
    todo: 背景切换后的展示还有些不自然

--- lib/app_root_container.dart
+++ lib/app_root_container.dart
@@ -6,10 +6,10 @@ import 'package:princess_garden_client/business/scene/atom/atom.dart';
 import 'package:princess_garden_client/business/scene/snowfall/snowflakes.dart';
 import 'package:princess_garden_client/business/scene/starlight/starlight.dart';
 import 'package:princess_garden_client/business/scene/the_eye_of_the_deep_sea/the_eye_of_the_deep_sea.dart';
+import 'package:princess_garden_client/business/transition/requiem_transition.dart';
 
 import 'base/background_widget.dart';
 import 'business/scene/dizzy_rotation/dizzy_rotation.dart';
-import 'business/scene/snowfall/sky_background.dart';
 
 class AppRootContainer extends StatefulWidget {
   const AppRootContainer({super.key});
@@ -22,7 +22,8 @@ class _AppRootContainerState extends State<AppRootContainer> {
   late List<BackgroundWidget> _backgroundWidgets;
   int _currentIndex = 0;
   final Random _random = Random();
-  double _overlayOpacity = 0;
+  late Duration _sceneDuration;
+  late Duration _transitionDuration;
 
   @override
   void initState() {
@@ -49,14 +50,28 @@ class _AppRootContainerState extends State<AppRootContainer> {
         duration: const Duration(milliseconds: 5300),
       ),
     ];
-    _startTimer(_backgroundWidgets[_currentIndex].duration);
+
+    _switchBackground();
   }
 
   void _switchBackground() {
     setState(() {
       _currentIndex = _random.nextInt(_backgroundWidgets.length);
+      _sceneDuration = _backgroundWidgets[_currentIndex].duration;
+      _transitionDuration = const Duration(milliseconds: 5253);
+
+      debugPrint("_currentIndex: $_currentIndex");
+      debugPrint("_sceneDuration: $_sceneDuration");
+      debugPrint("_transitionDuration: $_transitionDuration");
+      debugPrint("--------------------------");
     });
-    _startTimer(_backgroundWidgets[_currentIndex].duration);
+
+    Future.delayed(
+      _sceneDuration +
+          _transitionDuration +
+          const Duration(seconds: 2, milliseconds: 530),
+      _switchBackground,
+    );
   }
 
   @override
@@ -64,33 +79,18 @@ class _AppRootContainerState extends State<AppRootContainer> {
     return Scaffold(
       body: Stack(
         children: [
-          const Positioned.fill(child: SkyBackground()),
           // 注意这里一定要有约束，不然CustomPainter拿到的size和width都是0
-          Positioned.fill(child: _backgroundWidgets[_currentIndex].widget),
-          AnimatedOpacity(
-            opacity: _overlayOpacity,
-            duration: const Duration(milliseconds: 5300),
-            child: Container(
-              color: Colors.white.withOpacity(0.53),
-            ),
+          Positioned.fill(
+            key: Key((_currentIndex + _random.nextInt(999)).toString()),
+            child: _backgroundWidgets[_currentIndex].widget,
+          ),
+          RequiemTransition(
+            key: Key((_currentIndex + _random.nextInt(999)).toString()),
+            sceneDuration: _sceneDuration,
+            transitionDuration: _transitionDuration,
           ),
         ],
       ),
     );
-  }
-
-  void _startTimer(Duration sceneDuration,
-      [Duration transitionDuration = const Duration(milliseconds: 5300)]) {
-    Future.delayed(sceneDuration, () {
-      setState(() {
-        _overlayOpacity = 1;
-      });
-      Future.delayed(transitionDuration, () {
-        setState(() {
-          _overlayOpacity = 0;
-        });
-        _switchBackground();
-      });
-    });
   }
 }
--- lib/business/transition/requiem_transition.dart
+++ lib/business/transition/requiem_transition.dart
@@ -2,22 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 class RequiemTransition extends StatelessWidget {
-  const RequiemTransition({Key? key}) : super(key: key);
+  const RequiemTransition(
+      {Key? key, required this.sceneDuration, required this.transitionDuration})
+      : super(key: key);
+
+  final Duration sceneDuration;
+  final Duration transitionDuration;
 
   @override
   Widget build(BuildContext context) {
     final MovieTween tween = MovieTween()
-      ..scene(
-              begin: const Duration(milliseconds: 0),
-              end: const Duration(milliseconds: 5253))
+      ..scene(begin: Duration.zero, end: sceneDuration)
+          .tween("opacity", Tween(begin: 0.0, end: 0.0))
+      ..scene(begin: sceneDuration, end: sceneDuration + transitionDuration)
           .tween("opacity", Tween(begin: 0.0, end: 1.0))
       ..scene(
-              begin: const Duration(milliseconds: 5253),
-              end: const Duration(seconds: 2, milliseconds: 5253))
+              begin: sceneDuration + transitionDuration,
+              end: sceneDuration +
+                  transitionDuration +
+                  const Duration(seconds: 2))
           .tween("opacity", Tween(begin: 1.0, end: 1.0))
       ..scene(
-              begin: const Duration(seconds: 2, milliseconds: 5253),
-              end: const Duration(seconds: 2, milliseconds: 5253 + 253))
+              begin: sceneDuration +
+                  transitionDuration +
+                  const Duration(seconds: 2),
+              end: sceneDuration +
+                  transitionDuration +
+                  const Duration(seconds: 2, milliseconds: 253))
           .tween("opacity", Tween(begin: 1.0, end: 0.0));
 
     return Positioned.fill(
