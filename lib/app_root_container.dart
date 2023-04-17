c9af24c9db85392295a0f18d9d5cad12186b6d54
crablet <love4uandgzfc@gmail.com>
2023-04-18 01:15:22 +0800

    feat: 初步实现切换过渡背景的功能
    todo: 还没用上RequiemTransition，看看如何配合使用

--- lib/app_root_container.dart
+++ lib/app_root_container.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:princess_garden_client/business/scene/atom/atom.dart';
 import 'package:princess_garden_client/business/scene/snowfall/snowflakes.dart';
@@ -18,6 +21,8 @@ class AppRootContainer extends StatefulWidget {
 class _AppRootContainerState extends State<AppRootContainer> {
   late List<BackgroundWidget> _backgroundWidgets;
   int _currentIndex = 0;
+  final Random _random = Random();
+  double _overlayOpacity = 0;
 
   @override
   void initState() {
@@ -44,24 +49,48 @@ class _AppRootContainerState extends State<AppRootContainer> {
         duration: const Duration(milliseconds: 5300),
       ),
     ];
+    _startTimer(_backgroundWidgets[_currentIndex].duration);
   }
 
   void _switchBackground() {
     setState(() {
-      _currentIndex = (_currentIndex + 1) % _backgroundWidgets.length;
+      _currentIndex = _random.nextInt(_backgroundWidgets.length);
     });
+    _startTimer(_backgroundWidgets[_currentIndex].duration);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children:  [
+        children: [
           const Positioned.fill(child: SkyBackground()),
           // 注意这里一定要有约束，不然CustomPainter拿到的size和width都是0
           Positioned.fill(child: _backgroundWidgets[_currentIndex].widget),
+          AnimatedOpacity(
+            opacity: _overlayOpacity,
+            duration: const Duration(milliseconds: 5300),
+            child: Container(
+              color: Colors.white.withOpacity(0.53),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  void _startTimer(Duration sceneDuration,
+      [Duration transitionDuration = const Duration(milliseconds: 5300)]) {
+    Future.delayed(sceneDuration, () {
+      setState(() {
+        _overlayOpacity = 1;
+      });
+      Future.delayed(transitionDuration, () {
+        setState(() {
+          _overlayOpacity = 0;
+        });
+        _switchBackground();
+      });
+    });
   }
 }
